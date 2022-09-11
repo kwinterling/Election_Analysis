86c3709c3722b0e7d897b6770ed67af22ae3be01
Karl Winterling <kwinterling@gmail.com>
2022-09-11 14:00:33 -0700

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -10,17 +10,99 @@ import os
 
 file_to_load = os.path.join("Resources", "election_results.csv")
 
+# Output file
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# 1. Initialize a vote counter
+
+total_votes = 0
+
+# Candidate options
+
+candidate_options = []
+
+# Dictionary for candidate votes
+
+candidate_votes = {}
+
+# Winning candidate tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open and read election results
 
 with open(file_to_load) as election_data:
     # TO DO: Analysis
     file_reader = csv.reader(election_data)
 
-    # Print the header row
+    # Read the header row
     headers = next(file_reader)
-    print(headers)
+    
+    # Count rows
+    for row in file_reader:
+        # increment counter
+        total_votes += 1
 
-# Output file
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+        # Get the candidate name from each row
+        candidate_name = row[2]
 
+        # If the candidate does not match an existing candidate...
+        if candidate_name not in candidate_options:
+            # append to candidate_options list
+            candidate_options.append(candidate_name)
+
+            # begin tracking vote count
+            candidate_votes[candidate_name] = 0
+        
+        # tally votes
+        candidate_votes[candidate_name] += 1
+
+
+
+
+print(total_votes)
+
+# Print candidate list
+print(candidate_options)
+
+# Print vote tallies
+print(candidate_votes)
+
+# Determine the percentages of votes
+# Iterate through candidates
+for candidate_name in candidate_votes:
+    # Retrieve vote count
+    votes = candidate_votes[candidate_name]
+    # calculate the percentage of votes
+    vote_percentage = float(votes) / float(total_votes) * 100
+
+    # format percentage, see https://stackoverflow.com/questions/2389846/python-decimals-format#2390047
+    formatted_percentage = '%.3g'%(vote_percentage)
+
+    # add decimal if rounded to whole number
+    if '.' not in formatted_percentage:
+        formatted_percentage = formatted_percentage + ".0"
+
+    # Print candidate name and percentage of votes
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+    # Determine winning vote count and candidate
+    # Determine if votes is greater than winning count
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        # If true, set winning_count = votes and winning_percentage = vote_percentage
+        winning_count = votes
+        winning_percentage = vote_percentage
+        # set winning candidate to current candidate name
+        winning_candidate = candidate_name
+
+
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}\n"
+    f"-------------------------\n")
+
+print(winning_candidate_summary)
 
